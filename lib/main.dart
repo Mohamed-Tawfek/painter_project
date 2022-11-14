9a5cec07395ad57adbf851c2ebb604f2f8539d97
Mohamed-Tawfek <mohamed.tawfek.eng@gmail.com>
2022-11-14 09:29:09 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,301 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:lottie/lottie.dart';
+import 'package:scribble/scribble.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 4)).then((value) =>
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (_)=>const HomePage()), (route) => false));
+
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset('assets/animated_json.json'),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ScribbleNotifier notifier;
+
+  @override
+  void initState() {
+    notifier = ScribbleNotifier();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 1,
+          child: Stack(
+            children: [
+              Scribble(
+                notifier: notifier,
+                drawPen: true,
+              ),
+              Positioned(
+                top: 16,
+                right: 16,
+                child: Column(
+                  children: [
+                    _buildColorToolbar(context),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    _buildStrokeToolbar(context),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStrokeToolbar(BuildContext context) {
+    return StateNotifierBuilder<ScribbleState>(
+      stateNotifier: notifier,
+      builder: (context, state, _) => Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          for (final w in notifier.widths)
+            _buildStrokeButton(
+              context,
+              strokeWidth: w,
+              state: state,
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStrokeButton(
+    BuildContext context, {
+    required double strokeWidth,
+    required ScribbleState state,
+  }) {
+    final selected = state.selectedWidth == strokeWidth;
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: Material(
+        elevation: selected ? 4 : 0,
+        shape: const CircleBorder(),
+        child: InkWell(
+          onTap: () => notifier.setStrokeWidth(strokeWidth),
+          customBorder: const CircleBorder(),
+          child: AnimatedContainer(
+            duration: kThemeAnimationDuration,
+            width: strokeWidth * 2,
+            height: strokeWidth * 2,
+            decoration: BoxDecoration(
+                color: state.map(
+                  drawing: (s) => Color(s.selectedColor),
+                  erasing: (_) => Colors.transparent,
+                ),
+                border: state.map(
+                  drawing: (_) => null,
+                  erasing: (_) => Border.all(width: 1),
+                ),
+                borderRadius: BorderRadius.circular(50.0)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildColorToolbar(BuildContext context) {
+    return StateNotifierBuilder<ScribbleState>(
+      stateNotifier: notifier,
+      builder: (context, state, _) => Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _buildUndoButton(context),
+          const Divider(
+            height: 4.0,
+          ),
+          _buildRedoButton(context),
+          const Divider(
+            height: 4.0,
+          ),
+          _buildClearButton(context),
+          const Divider(
+            height: 20.0,
+          ),
+          _buildPointerModeSwitcher(context,
+              penMode:
+                  state.allowedPointersMode == ScribblePointerMode.penOnly),
+          const Divider(
+            height: 20.0,
+          ),
+          _buildEraserButton(context, isSelected: state is Erasing),
+          _buildColorButton(context, color: Colors.black, state: state),
+          _buildColorButton(context, color: Colors.red, state: state),
+          _buildColorButton(context, color: Colors.green, state: state),
+          _buildColorButton(context, color: Colors.blue, state: state),
+          _buildColorButton(context, color: Colors.teal, state: state),
+          _buildColorButton(context, color: Colors.amber, state: state),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPointerModeSwitcher(BuildContext context,
+      {required bool penMode}) {
+    return FloatingActionButton.small(
+      heroTag: 1,
+      onPressed: () => notifier.setAllowedPointersMode(
+        penMode ? ScribblePointerMode.all : ScribblePointerMode.penOnly,
+      ),
+      tooltip:
+          "Switch drawing mode to ${penMode ? "all pointers" : "pen only"}",
+      child: AnimatedSwitcher(
+        duration: kThemeAnimationDuration,
+        child: !penMode
+            ? const Icon(
+                Icons.touch_app,
+                key: ValueKey(true),
+              )
+            : const Icon(
+                Icons.do_not_touch,
+                key: ValueKey(false),
+              ),
+      ),
+    );
+  }
+
+  Widget _buildEraserButton(BuildContext context, {required bool isSelected}) {
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: FloatingActionButton.small(
+        heroTag: 2,
+        tooltip: "Erase",
+        backgroundColor: const Color(0xFFF7FBFF),
+        elevation: isSelected ? 10 : 2,
+        shape: !isSelected
+            ? const CircleBorder()
+            : RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+        onPressed: notifier.setEraser,
+        child: const Icon(Icons.remove, color: Colors.blueGrey),
+      ),
+    );
+  }
+
+  Widget _buildColorButton(
+    BuildContext context, {
+    required Color color,
+    required ScribbleState state,
+  }) {
+    final isSelected = state is Drawing && state.selectedColor == color.value;
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: FloatingActionButton.small(
+        heroTag: 3,
+          backgroundColor: color,
+          elevation: isSelected ? 10 : 2,
+          splashColor: Colors.black,
+          shape: !isSelected
+              ? const CircleBorder()
+              : RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+          child: Container(
+            child: isSelected ? const Icon(Icons.check_outlined) : null,
+          ),
+          onPressed: () => notifier.setColor(color)),
+    );
+  }
+
+  Widget _buildUndoButton(
+    BuildContext context,
+  ) {
+    return FloatingActionButton.small(
+      heroTag: 4,
+      tooltip: "Undo",
+      onPressed: notifier.canUndo ? notifier.undo : null,
+      disabledElevation: 0,
+      backgroundColor: notifier.canUndo ? Colors.blueGrey : Colors.grey,
+      child: const Icon(
+        Icons.undo_rounded,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget _buildRedoButton(
+    BuildContext context,
+  ) {
+    return FloatingActionButton.small(
+      heroTag: 5,
+      tooltip: "Redo",
+      onPressed: notifier.canRedo ? notifier.redo : null,
+      disabledElevation: 0,
+      backgroundColor: notifier.canRedo ? Colors.blueGrey : Colors.grey,
+      child: const Icon(
+        Icons.redo_rounded,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget _buildClearButton(BuildContext context) {
+    return FloatingActionButton.small(
+      heroTag: 6,
+      tooltip: "Clear",
+      onPressed: notifier.clear,
+      disabledElevation: 0,
+      backgroundColor: Colors.blueGrey,
+      child: const Icon(Icons.clear),
+    );
+  }
+}
